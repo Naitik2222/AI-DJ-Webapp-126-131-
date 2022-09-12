f68cfb24a3724c54f29b8d38ac42516be6e24bc8
Naitik Karn <84630339+Naitik2222@users.noreply.github.com>
2022-09-12 20:09:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-function setup(){
-    canvas=createCanvas(550,550);
-    canvas.position(400,250);
+song="";
 
+function preload(){
+    song= loadSound("song.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video , 0,0,600,500);
+  textSize(20);
+    stroke("yellow")
+    let d = day();
+text('Current date: \n' + d, 15, 50);
+
+let m = month();
+text(': \n' + m, 55, 50);
+
+
+stroke("#f5c61b")
+let y = year();
+text(': \n' + y, 70, 50);
+}
+
+function Start(){
+    song.play();
 }
