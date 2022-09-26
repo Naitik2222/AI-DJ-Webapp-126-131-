d215e2a583ce06870f763dcfbfbe8da00827c65d
Naitik Karn <84630339+Naitik2222@users.noreply.github.com>
2022-09-26 18:51:27 +0530

    Add files via upload

--- main (1).js
+++ main (1).js
@@ -1,11 +1,19 @@
-song="";
+
 leftWristX=0;
 leftWristY =0;
 rightWristX=0;
 rightWristY =0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+song_unstoppable = "";
+song_go_down_deh= "";
 
 function preload(){
     song= loadSound("song.mp3");
+}
+function preload(){
+    unstoppable_song = loadSound("unstoppable.mp3");
+    go_down_deh_song = loadSound("go_down_deh.mp3");
 }
 function setup(){
     canvas=createCanvas(600,500);
@@ -24,6 +32,13 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
        console.log(results);
+       
+       scoreLeftWrist= results[0].pose.keypoints[9].score;
+       console.log("scoreLeftWrist="+scoreLeftWrist);
+   
+       scoreRightWrist= results[0].pose.keypoints[10].score;
+       console.log("scoreRightWrist="+scoreRightWrist);
+   
        leftWristX = results[0].pose.leftWrist.x;
        leftWristY = results[0].pose.leftWrist.y;
        console.log("leftWristX ="+leftWristX+ "leftWristY="+leftWristY);
@@ -37,22 +52,34 @@ function gotPoses(results){
 }
     function draw(){
     image(video , 0,0,600,500);
-  textSize(20);
-    stroke("yellow")
-    let d = day();
-text('Current date: \n' + d, 15, 50);
+    song_unstoppable = unstoppable_song.isPlaying();
+    console.log(song_unstoppable);
 
-let m = month();
-text(': \n' + m, 55, 50);
+    song_go_down_deh= go_down_deh_song.isPlaying();
+    console.log(song_go_down_deh);
 
+    if(scoreLeftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        unstoppable_song.stop();
+        if(song_go_down_deh == false){
+            go_down_deh_song.play();
+        }
+        else{
+            console.log("Song Name: Go down Deh Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Go down deh Song";
+        }
+    }
 
-stroke("#f5c61b")
-let y = year();
-text(': \n' + y, 70, 50);
+    if(scoreRightWrist > 0.2){
+        circle(rightWrist_x,rightWrist_y,20);
+        go_down_deh_song.stop();
+        if(song_unstoppable == false){
+            unstoppable_song.play();
+        }
+        else{
+            console.log("Song Name: Unstoppable Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Unstoppable Theme Song";
+        }
+    }
 }
 
-function Start(){
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
-}
